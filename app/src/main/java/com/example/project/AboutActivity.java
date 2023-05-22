644f98e5c4dd9f101e73af9810a0aac256613c9d
Chris <b22chrmo@student.his.se>
2023-05-22 17:54:01 +0200

    Added a text in the about page. Added a button in the about page so the user can return.

--- app/src/main/java/com/example/project/AboutActivity.java
+++ app/src/main/java/com/example/project/AboutActivity.java
@@ -11,7 +11,6 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-        setSupportActionBar(toolbar);
     }
 
 }
