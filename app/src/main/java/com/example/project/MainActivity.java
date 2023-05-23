af1aa6b4ded00e84a2fff4e184dcafd821a80ab5
Chris <b22chrmo@student.his.se>
2023-05-23 17:51:35 +0200

    Added a RecyclerViewAdapter.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,54 @@
 package com.example.project;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=b22chrmo";
+
+    ArrayList<ExtinctItem> extList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        new JsonTask(this).execute(JSON_URL);
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+        Log.d("MainActivity", json);
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<ExtinctItem>>() {}.getType();
+        extList = gson.fromJson(json, type);
+
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, extList, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(ExtinctItem item) {
+                Toast.makeText(MainActivity.this, item.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RecyclerView view = findViewById(R.id.recycler_view);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        adapter.notifyDataSetChanged();
+        view.setAdapter(adapter);
     }
 
 }
