ece718dfa424d69078d439bce4803eec99cd4ae6
Chris <b22chrmo@student.his.se>
2023-05-24 16:13:51 +0200

    Added a gettext method to add text to the TextView in the about page.

--- app/src/main/java/com/example/project/AboutActivity.java
+++ app/src/main/java/com/example/project/AboutActivity.java
@@ -4,16 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class AboutActivity extends AppCompatActivity {
 
+    TextView aboutText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        aboutText = findViewById(R.id.textView);
+        aboutText.setText(getAboutText());
 
         Button button = (Button) findViewById(R.id.button);
 
@@ -24,6 +29,13 @@ public class AboutActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public String getAboutText() {
+        return "This app was made for those who are interested in the animals that unfortunately went extinct. " +
+                "Even if you're a beginner in this area and you're just curious about these beautiful animals, " +
+                "if you're wondering when these animals used to roam the planet, or which family they belong to," +
+                " this app was made for you!";
     }
 
 }
